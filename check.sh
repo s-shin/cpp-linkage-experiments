a2079fb9a892c88e2e99436f15e7dda9bb5faa6c
Shintaro Seki <s2pch.luck@gmail.com>
2017-08-15 17:11:03 +0900

    Remove impossible parts etc.

--- check.sh
+++ check.sh
@@ -5,25 +5,16 @@ for libhello_lib_type in SHARED STATIC; do
   for libhello_libsay_ver in v1 v2; do
     for libhello_visibility in on off; do
       for myapp_libsay_ver in v1 v2; do
-        for myapp_static_link_of_shared_libhello in on off; do
-          if [[ "$(uname)" != 'Linux' ]]; then
-            myapp_static_link_of_shared_libhello=na
-          fi
-          dir="build-${libhello_lib_type}-${libhello_libsay_ver}-${libhello_visibility}-${myapp_libsay_ver}-${myapp_static_link_of_shared_libhello}"
-          mkdir -p "$dir"
-          pushd "$dir"
-          cmake .. -GNinja \
-            -DLIBHELLO_LIB_TYPE="${libhello_lib_type}" \
-            -DLIBHELLO_LIBSAY_VER="${libhello_libsay_ver}" \
-            -DLIBHELLO_VISIBILITY="${libhello_visibility}" \
-            -DMYAPP_LIBSAY_VER="${myapp_libsay_ver}" \
-            -DMYAPP_STATIC_LINK_OF_SHARED_LIBHELLO="${myapp_static_link_of_shared_libhello}"
-          ninja
-          popd
-          if [[ "${myapp_static_link_of_shared_libhello}" = 'na' ]]; then
-            break
-          fi
-        done
+        dir="build-${libhello_lib_type}-${libhello_libsay_ver}-${libhello_visibility}-${myapp_libsay_ver}"
+        mkdir -p "$dir"
+        pushd "$dir"
+        cmake .. -GNinja \
+          -DLIBHELLO_LIB_TYPE="${libhello_lib_type}" \
+          -DLIBHELLO_LIBSAY_VER="${libhello_libsay_ver}" \
+          -DLIBHELLO_VISIBILITY="${libhello_visibility}" \
+          -DMYAPP_LIBSAY_VER="${myapp_libsay_ver}"
+        ninja
+        popd
       done
     done
   done
